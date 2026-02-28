2195fe9676dbc6df660b0adf729955ccc32d796b
Philipp Reiter <p.reiter93@gmail.com>
2026-02-28 17:29:31 +0100

    add unit tests

--- src/skeleton/mod.rs
+++ src/skeleton/mod.rs
@@ -8,10 +8,8 @@ use crate::skeleton::workspace::reduce_workspace_by_member;
 use crate::OptimisationProfile;
 use anyhow::Context;
 use cargo_manifest::Product;
-use cargo_metadata::Metadata;
 use fs_err as fs;
 use globwalk::GlobWalkerBuilder;
-use pathdiff::diff_paths;
 use serde::{Deserialize, Serialize};
 use std::path::{Path, PathBuf};
 
@@ -319,46 +317,4 @@ fn extract_cargo_metadata(path: &Path) -> Result<cargo_metadata::Metadata, anyho
     cmd.no_deps();
 
     cmd.exec().context("Cannot extract Cargo metadata")
-}
-
-/// If the top-level `Cargo.toml` has a `members` field, replace it with
-/// a list consisting of just the path to the package.
-///
-/// Also deletes the `default-members` field because it does not play nicely
-/// with a modified `members` field and has no effect on cooking the final recipe.
-fn ignore_all_members_except(
-    manifests: &mut [ParsedManifest],
-    metadata: &Metadata,
-    member: String,
-) {
-    let workspace_toml = manifests
-        .iter_mut()
-        .find(|manifest| manifest.relative_path == std::path::Path::new("Cargo.toml"));
-
-    if let Some(workspace) = workspace_toml.and_then(|toml| toml.contents.get_mut("workspace")) {
-        if let Some(members) = workspace.get_mut("members") {
-            let workspace_root = &metadata.workspace_root;
-            let workspace_packages = metadata.workspace_packages();
-
-            if let Some(pkg) = workspace_packages
-                .into_iter()
-                .find(|pkg| pkg.name == member)
-            {
-                // Make this a relative path to the workspace, and remove the `Cargo.toml` child.
-                let member_cargo_path = diff_paths(pkg.manifest_path.as_os_str(), workspace_root);
-                let member_workspace_path = member_cargo_path
-                    .as_ref()
-                    .and_then(|path| path.parent())
-                    .and_then(|dir| dir.to_str());
-
-                if let Some(member_path) = member_workspace_path {
-                    *members =
-                        toml::Value::Array(vec![toml::Value::String(member_path.to_string())]);
-                }
-            }
-        }
-        if let Some(workspace) = workspace.as_table_mut() {
-            workspace.remove("default-members");
-        }
-    }
 }
